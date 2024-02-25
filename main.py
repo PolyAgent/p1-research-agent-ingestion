e5009aab24391899661c20637d81574d628bd414
antp <anton.polishko@gmail.com>
2024-02-25 09:57:30 -0800

    add basic arxiv paper injestion

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import logging
-import logging.handlers
 import os
 from datetime import datetime
-
+import pymongo
 import requests
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+
+from typing import List
+
+
+MAX_NUMBER_PAPERS = 200 #number of papers to pull
+
+def ingest_arxiv_papers():
+    import paperscraper
+    papers_df = paperscraper.arxiv.get_arxiv_papers("cat:cs.AI", max_results=MAX_NUMBER_PAPERS, search_options={'sort_by': paperscraper.arxiv.arxiv.SortCriterion.SubmittedDate})    
+    
+    db = client.arxiv
+    collection = db['test_papers']
+    records = papers_df.to_dict(orient='records')
+    collection.insert_many(records)
+    return papers_df 
+
 
 try:
     SOME_SECRET = os.environ["SOME_SECRET"]
@@ -26,10 +30,6 @@ except KeyError:
 
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    papers = ingest_arxiv_papers()
     webhook_url = 'https://hooks.zapier.com/hooks/catch/6996241/3ej8cwi/'
-    r = requests.post(webhook_url, json={'time': str(datetime.now())})
-    if r.status_code == 200:
-        logger.info(f'Sucess: YES')
+    r = requests.post(webhook_url, json={'time': str(datetime.now()), 'num_papers': len(papers)})
