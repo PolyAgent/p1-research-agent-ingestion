7f18063d131c5e62badcdf02984e0b7415e3621a
antp <anton.polishko@gmail.com>
2024-03-05 23:32:55 -0800

    split arxiv pull per category
    To mitigate the issue of irregularities in the paper order returned from arxiv when you use "cat:cs.AI OR cat:cs.CL"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys, traceback
 from datetime import datetime
 import pymongo
 import requests
+import paperscraper
 
 client = pymongo.MongoClient(os.environ['MONGODB_URI'])
 
@@ -11,23 +12,24 @@ from typing import List
 MAX_NUMBER_PAPERS = 200 #number of papers to pull
 
 def ingest_arxiv_papers():
-    import paperscraper
-    papers_df = paperscraper.arxiv.get_arxiv_papers("cat:cs.AI OR cat:cs.LG OR cat:cs.CL", max_results=MAX_NUMBER_PAPERS, search_options={'sort_by': paperscraper.arxiv.arxiv.SortCriterion.SubmittedDate})    
-    # add url
-    papers_df['url'] = papers_df["doi"].apply(lambda row: "https://arxiv.org/abs/"+row.split("/arXiv.")[1])
-    papers_df['pdf_url'] = papers_df["doi"].apply(lambda row: "https://arxiv.org/pdf/"+row.split("/arXiv.")[1]+".pdf") 
-    
-    db = client.arxiv
-    collection = db['papers_for_review']
-    records = papers_df.to_dict(orient='records')
+    categories_to_pull = ['cs.AI', 'cs.LG', 'cs.CL']
     count = 0
-    for i,record in enumerate(records):
-        try:
-            print(record['doi'])
-            collection.insert_one(record)
-            count +=1
-        except:
-            print("duplicate found", record['doi'])
+    for cat in categories_to_pull:
+        papers_df = paperscraper.arxiv.get_arxiv_papers(f"cat:{cat}", max_results=MAX_NUMBER_PAPERS, search_options={'sort_by': paperscraper.arxiv.arxiv.SortCriterion.SubmittedDate})    
+        # add url
+        papers_df['url'] = papers_df["doi"].apply(lambda row: "https://arxiv.org/abs/"+row.split("/arXiv.")[1])
+        papers_df['pdf_url'] = papers_df["doi"].apply(lambda row: "https://arxiv.org/pdf/"+row.split("/arXiv.")[1]+".pdf") 
+        
+        db = client.arxiv
+        collection = db['papers_for_review']
+        records = papers_df.to_dict(orient='records')
+        for i,record in enumerate(records):
+            try:
+                print(record['doi'])
+                collection.insert_one(record)
+                count +=1
+            except:
+                print("duplicate found", record['doi'])
     return count
 
 if __name__ == "__main__":
